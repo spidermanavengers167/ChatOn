426d6f1f64d0fa2f1b0eb2251ff57f2b87fef4cd
spidermanavengers167 <121699091+spidermanavengers167@users.noreply.github.com>
2023-08-13 11:22:41 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyANErtwa557nSIjOBLm9NPUBsnl7neYEXc",
+    authDomain: "chaton-ed542.firebaseapp.com",
+    projectId: "chaton-ed542",
+    storageBucket: "chaton-ed542.appspot.com",
+    messagingSenderId: "1014426116692",
+    appId: "1:1014426116692:web:32da33c5492a6f6f71c3c1"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
